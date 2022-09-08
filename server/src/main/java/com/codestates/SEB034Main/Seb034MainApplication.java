f189727f115d033c1f1cd1bea982f65389e6934e
jyoungkim <iamjyoungkim@gmail.com>
2022-09-08 16:31:34 +0900

    Server Setting

---
+++ server/src/main/java/com/codestates/SEB034Main/Seb034MainApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.SEB034Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb034MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb034MainApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codestates/SEB034Main/Seb034MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.SEB034Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb034MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
