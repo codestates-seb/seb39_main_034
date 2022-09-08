2945af24fb53e6ac2b7e0e0f86cd5e5ff06eaca5
jyoungkim <iamjyoungkim@gmail.com>
2022-09-08 16:47:14 +0900

    Sever Setting v1.5

---
+++ server/src/main/java/com/codestates/SEB034Main/testcall/TestController.java
@@ -0,0 +1,24 @@
+package com.codestates.SEB034Main.testcall;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/v1")
+@RestController
+public class TestController {
+
+    @GetMapping("/testcall")
+    public ResponseEntity testCall() {
+        Map<String, String> testmap = new HashMap<>();
+        testmap.put("testcall1", "ok");
+        testmap.put("testcall2", "ok");
+
+        return new ResponseEntity(testmap, HttpStatus.OK);
+    }
+}
