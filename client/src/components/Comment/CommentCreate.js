1a5b9cf61486e3d62417941817153856a14b2612
DOSI <dosira686@naver.com>
2022-09-22 14:46:24 +0900

    feat: add emoji

--- client/src/components/Comment/CommentCreate.js
+++ client/src/components/Comment/CommentCreate.js
@@ -1,11 +1,15 @@
 import { CommentCreateContainer } from './CommentStyle'
-import { Input } from '../../styles/globalStyles'
+// import { Input } from '../../styles/globalStyles'
 import { CompleteBtn } from '../Widget/WidgetStyle'
+import InputEmoji from 'react-input-emoji'
+import { useState } from 'react'
 
 export default function CommentCreate() {
+  const [text, setText] = useState('')
   return (
     <CommentCreateContainer>
-      <Input placeholder="텍스트 댓글" />
+      <InputEmoji value={text} onChange={setText} placeholder="텍스트 댓글" />
+      {/* <Input placeholder="텍스트 댓글" /> */}
       <CompleteBtn type="submit">생성하기</CompleteBtn>
     </CommentCreateContainer>
   )
