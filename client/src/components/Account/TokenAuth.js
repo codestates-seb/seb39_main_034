b8af2fa2965aa75ebeab87d71abfde7c1d417493
couchSol <sollee922@gmail.com>
2022-09-26 16:42:52 +0900

    refactor: fix onLogout function

--- client/src/components/Account/TokenAuth.js
+++ client/src/components/Account/TokenAuth.js
@@ -7,11 +7,12 @@ import {
   removeCookieToken,
 } from '../../data/Cookie'
 
-const refresh_token = getCookieToken()
 const access_max_age = 24 * 3600 * 1000
 
 export const onLoginSuccess = (res) => {
   const dispatch = useDispatch()
+
+  // token 세팅
   dispatch(
     SET_LOGIN({
       userName: res.username,
@@ -20,7 +21,7 @@ export const onLoginSuccess = (res) => {
   )
   setRefreshToken(res.json.refresh_token)
 
-  // accessToken 설정
+  // header에 accessToken 설정
   axios.defaults.headers.common['Authorization'] = `Bearer ${res.data}`
 
   // accessToken 만료하기 1분 전에 로그인 연장
@@ -29,11 +30,21 @@ export const onLoginSuccess = (res) => {
 
 export const onLogout = () => {
   const dispatch = useDispatch()
+
+  // redux에서 accessToken 삭제
   dispatch(SET_LOGOUT())
+
+  // cookie에서 refreshToken 삭제
   removeCookieToken()
+
+  // header에서 accessToken 삭제
+  axios.defaults.headers.common['Authorization'] = null
 }
 
 export const onSilentRefresh = () => {
+  const refresh_token = getCookieToken()
+
+  // cookie에 refreshToken이 남아있다면 silent refresh 시도
   if (refresh_token) {
     axios
       .post('/silent-refresh', refresh_token)
