8bf4d36a88ee217d0358ea6c2335f61608bf06cc
couchSol <sollee922@gmail.com>
2022-09-27 12:59:53 +0900

    style: add landing page animations

--- client/src/components/Gnb/Gnb.js
+++ client/src/components/Gnb/Gnb.js
@@ -1,5 +1,6 @@
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import { Container, Col, Row } from '../../styles/globalStyles'
 import { SET_LOGIN, SET_LOGOUT } from '../../redux/authSlice'
 import { useState, useRef, useEffect } from 'react'
 import { FaTimes } from 'react-icons/fa'
@@ -67,64 +68,74 @@ function Gnb() {
   })
 
   return (
-    <NavContainer>
-      <div className="gnb-left">
-        <NavLogo to="/">
-          <NavIcon src="/logo.png" alt="logo" />
-        </NavLogo>
-        <NavMenu showMenu={showMenu}>
-          <NavLink style={navStyle} to="/main">
-            <li>글보기</li>
-          </NavLink>
-          <NavLink style={navStyle} to="/">
-            <li>명예의전당</li>
-          </NavLink>
-          <NavLink style={navStyle} to="/">
-            <li>이벤트</li>
-          </NavLink>
-          <button onClick={handleLogin}>임시 로그인 버튼</button>
-        </NavMenu>
-      </div>
-      <div className="gnb-right">
-        {/*로그인을 하지 않았을 경우*/}
-        <LoginMenu showMenu={showMenu} className="logout" isOpen={!isLogin}>
-          <NavLink style={navStyle} to="/login">
-            <li>로그인</li>
-          </NavLink>
-          <NavLink style={navStyle} to="/signup">
-            <li>회원가입</li>
-          </NavLink>
-        </LoginMenu>
-        {/*로그인을 했을 경우*/}
-        <LoginMenu className="login" isOpen={isLogin}>
-          <Profile
-            onClick={handleTooltipClick}
-            image={
-              'https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            }
-            author={'최강동안조안나'}
-          ></Profile>
-          <ProfileTooltip isOpen={showTooltip} ref={tooltipRef}>
-            <ul>
-              <li>
-                <button type="button" onClick={handleMypage}>
-                  마이페이지
-                </button>
-              </li>
-              <li>
-                <button type="button" onClick={handleLogout}>
-                  로그아웃
-                </button>
-              </li>
-            </ul>
-          </ProfileTooltip>
-        </LoginMenu>
-      </div>
-      {/*모바일 사이즈에서 메뉴 열림 버튼*/}
-      <MobileIcon onClick={handleMenuClick}>
-        {showMenu ? <FaTimes /> : <CgMenuRight />}
-      </MobileIcon>
-    </NavContainer>
+    <Container>
+      <Row>
+        <Col>
+          <NavContainer>
+            <div className="gnb-left">
+              <NavLogo to="/">
+                <NavIcon src="/logo.png" alt="logo" />
+              </NavLogo>
+              <NavMenu showMenu={showMenu}>
+                <NavLink style={navStyle} to="/main">
+                  <li>글보기</li>
+                </NavLink>
+                <NavLink style={navStyle} to="/">
+                  <li>명예의전당</li>
+                </NavLink>
+                <NavLink style={navStyle} to="/">
+                  <li>이벤트</li>
+                </NavLink>
+                <button onClick={handleLogin}>임시 로그인 버튼</button>
+              </NavMenu>
+            </div>
+            <div className="gnb-right">
+              {/*로그인을 하지 않았을 경우*/}
+              <LoginMenu
+                showMenu={showMenu}
+                className="logout"
+                isOpen={!isLogin}
+              >
+                <NavLink style={navStyle} to="/login">
+                  <li>로그인</li>
+                </NavLink>
+                <NavLink style={navStyle} to="/signup">
+                  <li>회원가입</li>
+                </NavLink>
+              </LoginMenu>
+              {/*로그인을 했을 경우*/}
+              <LoginMenu className="login" isOpen={isLogin}>
+                <Profile
+                  onClick={handleTooltipClick}
+                  image={
+                    'https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                  }
+                  author={'최강동안조안나'}
+                ></Profile>
+                <ProfileTooltip isOpen={showTooltip} ref={tooltipRef}>
+                  <ul>
+                    <li>
+                      <button type="button" onClick={handleMypage}>
+                        마이페이지
+                      </button>
+                    </li>
+                    <li>
+                      <button type="button" onClick={handleLogout}>
+                        로그아웃
+                      </button>
+                    </li>
+                  </ul>
+                </ProfileTooltip>
+              </LoginMenu>
+            </div>
+            {/*모바일 사이즈에서 메뉴 열림 버튼*/}
+            <MobileIcon onClick={handleMenuClick}>
+              {showMenu ? <FaTimes /> : <CgMenuRight />}
+            </MobileIcon>
+          </NavContainer>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
