99077cd66e280c52a1db3cd3c2969f6d8c3e1eac
couchSol <sollee922@gmail.com>
2022-10-05 10:46:57 +0900

    fix: push mypage down

--- client/src/components/MyPannel/MyPannelStyle.js
+++ client/src/components/MyPannel/MyPannelStyle.js
@@ -4,6 +4,7 @@ export const MyPannelContainer = styled.div`
   display: flex;
   width: 100%;
   height: 200px;
+  margin-top: 100px;
   > div.profile {
     display: flex;
     justify-content: center;
